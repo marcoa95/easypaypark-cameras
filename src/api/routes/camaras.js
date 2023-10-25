55b0a4782f45dd94a1cb583f3d8ee127588f267b
sicma795 <marcoa.cabrera95@gmail.com>
2023-10-24 18:53:21 -0600

    Add chunk logic

--- src/api/routes/camaras.js
+++ src/api/routes/camaras.js
@@ -3,17 +3,27 @@ const {
   createRecord,
 } = require('../controllers/recordsController');
 const errorHandler = require('../errors/errorHandler');
-const bodyParser = require('body-parser');
 
 const router = Router();
 
-router.post('/:name', bodyParser.xml(), (req, res) => {
+router.post('/:name', (req, res) => {
   const { name } = req.params;
-  const { EventNotificationAlert } = req.body;
 
-  createRecord(EventNotificationAlert, name)
-  .then(record => res.status(201).send({ record }))
-  .catch(err => errorHandler(res, err));
+  let rawData = Buffer.from('');
+
+  req.on('data', chunk => rawData += chunk)
+  .on('end', () => {
+    const data = rawData.toString();
+
+    console.log('DATA', data);
+
+    res.status(200).send('DATA: ' + data);
+
+    // createRecord(data, name)
+    // .then(record => res.status(201).send({ record }))
+    // .catch(err => errorHandler(res, err));
+  })
+  .on('error', err => errorHandler(res, err));
 });
 
 module.exports = router;
